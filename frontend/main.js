9dfefe4eff2a66210f819a8f0afdeec44f05c98e
cyber-Elvis <elvisifeanyi67@gmail.com>
2025-10-01 13:50:36 +1000

    Update frontend with real product and order API IPs

--- frontend/main.js
+++ frontend/main.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // These ports (30000 for Product, 30001 for Order) are mapped
     // from the Docker containers to the host machine in docker-compose.yml for Example 2.
     const PRODUCT_API_BASE_URL = 'http://172.188.196.47:8000';
-    const ORDER_API_BASE_URL = 'http://4.144.233.236:8001';
+    const ORDER_API_BASE_URL   = 'http://4.144.233.236:8001';
+
 
     // Product Service is named 'product-service-w04e2' and exposes port 8000 internally.
     //const PRODUCT_API_BASE_URL = 'http://product-service-w04e2:8000';
